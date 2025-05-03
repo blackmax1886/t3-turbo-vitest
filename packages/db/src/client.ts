7e4ec9fa30983f9d6ebde07d71c2304ba7cbc3ca
blackmax <blackmax1886@gmail.com>
2025-05-03 18:32:13 +0900

    chore: switch to local postgres

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,15 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
+if (!process.env.POSTGRES_URL) {
+  throw new Error("Missing POSTGRES_URL");
+}
+
+const queryClient = postgres(process.env.POSTGRES_URL);
 export const db = drizzle({
-  client: sql,
+  client: queryClient,
   schema,
   casing: "snake_case",
 });
