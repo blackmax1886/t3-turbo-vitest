6f90f36fdfa730eb1efe19ff6e0fe4e1a6715825
yorukuma <blackmax1886@gmail.com>
2025-09-13 16:36:35 +0900

    refactor(nextjs/vitest): add test-related deps directly to nextjs instead of vitest package

--- packages/vitest/src/react.ts
+++ packages/vitest/src/react.ts
@@ -1,11 +1,7 @@
 import path from "node:path"
-import { fileURLToPath } from "node:url"
 import baseConfig from "./base"
 import { defineConfig, mergeConfig } from "vitest/config"
 import react from "@vitejs/plugin-react-swc"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default mergeConfig(
     baseConfig,
@@ -13,7 +9,7 @@ export default mergeConfig(
         plugins: [react()],
         test: {
             environment: "jsdom",
-            setupFiles: [path.resolve(__dirname, "react-setup.ts")],
+            setupFiles: [path.resolve(process.cwd(), "__tests__/react-setup.ts")],
         },
         resolve: {
             alias: {
